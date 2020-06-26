0c67e52826b960630b41b3519985b9d41276cebc
SunPaz <matth.amoros@gmail.com>
2020-06-26 10:44:28 -0700

    Better formatting

--- network_plotter.py
+++ network_plotter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import json
 import matplotlib.pyplot as plt
@@ -5,6 +7,10 @@ import numpy as np
 from app.config import id_length, k_depth, min_contact, ip_address, answer_ping_behavior, interest_radius, verbose
 from app.kbucket import Kbucket
 from app.utils import *
+
+"""
+python3 network_plotter.py ffffffff
+"""
 
 execution_path = os.getcwd()
 kbucket_by_node_id = {}
@@ -25,6 +31,7 @@ if len(sys.argv) > 1:
 				kbucket_by_node_id[node] = Kbucket(node_id = node, id_length = len(node)/2)
 				kbucket_by_node_id[node].load_kbuckets()
 
+	""" Initilizations """
 	node_count = len(kbucket_by_node_id)
 	range = np.linspace(0, id_length*8, node_count, endpoint=True)
 	node_postion_by_id = {}
@@ -53,9 +60,9 @@ if len(sys.argv) > 1:
 					x_pos = (node_postion_by_id[known_node_id], node_postion_by_id[node_id])
 					y_pos = (compute_distance(source, known_node_id, id_length), compute_distance(source, node_id, id_length))
 					""" Plot """
-					plt.plot(x_pos, y_pos, marker='x')
+					plt.plot(x_pos, y_pos, marker='', linestyle='--')
 
-	plt.plot(range, distances, marker='.')
+	plt.plot(range, distances, marker='o', linestyle='')
 	plt.title("Connection POV " + source)
 	""" Plot center """
 	plt.legend(labels)
